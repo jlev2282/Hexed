23d28937725e2329321c49d82300bcb530b86cce
Adam Kearney <Adam Kearney>
2017-04-12 18:20:41 -0400

    updated design

--- assets/javascript/pop_up.js
+++ assets/javascript/pop_up.js
@@ -10,6 +10,7 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
+    
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -18,8 +19,8 @@ span.onclick = function() {
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
